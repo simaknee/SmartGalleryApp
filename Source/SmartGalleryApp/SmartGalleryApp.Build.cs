edecc0a6c621676e88bea5cded3af816ff20f152
simaknee <sammy9625@gmail.com>
2025-03-05 22:28:15 +0900

    [Build] Neural network module and android build option added

--- Source/SmartGalleryApp/SmartGalleryApp.Build.cs
+++ Source/SmartGalleryApp/SmartGalleryApp.Build.cs
@@ -8,16 +8,28 @@ public class SmartGalleryApp : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NNE", "ImageWrapper", "RenderCore", "RHI", "SlateCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AssetRegistry", "ApplicationCore" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PrivateDependencyModuleNames.AddRange(new string[]
+            {
+                "Launch",
+                "AndroidRuntimeSettings",
+                "AndroidPermission",
+            });
+
+            // Android JNI 연동 활성화
+            PrivateIncludePaths.Add("SmartGalleryApp/Private");
+        }
+    }
 }
