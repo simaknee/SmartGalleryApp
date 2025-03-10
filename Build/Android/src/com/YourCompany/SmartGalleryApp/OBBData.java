cf936a75a9339553847818c461a93bfa48729209
simaknee <sammy9625@gmail.com>
2025-03-10 12:08:26 +0900

    [Feat] Added category managing widget blueprint

--- Build/Android/src/com/YourCompany/SmartGalleryApp/OBBData.java
+++ Build/Android/src/com/YourCompany/SmartGalleryApp/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-51540079L // the length of the file in bytes
+51568158L // the length of the file in bytes
 )
 };
 };
